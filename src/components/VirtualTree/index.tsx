43ffa1ca8e567d00152fa4e3deb73384c7f4ee37
ImCccc <306022598@qq.com>
2024-07-03 17:54:30 +0800

    sdfdsf

--- src/components/VirtualTree/index.tsx
+++ src/components/VirtualTree/index.tsx
@@ -1,11 +1,32 @@
+import { useMemo } from "react";
 import VirtualList, { TableProps } from "../VirtualList";
 
 export type VirtualTreeProps = {
   childrenKey?: string;
 };
 
-const Comp: React.FC<TableProps & VirtualTreeProps> = (props) => {
-  return <VirtualList {...props} />;
+// 数组打平
+const flatten = (list: any[], allList = [], _level = 0) => {
+  return [
+    ...allList,
+    ...list.reduce((all, { children, ...item }) => {
+      all.push({ ...item, _level });
+      if (children && children.length) {
+        flatten(children, allList, _level + 1);
+      }
+      return all;
+    }, allList),
+  ];
+};
+
+const Comp: React.FC<TableProps & VirtualTreeProps> = ({ list, ...props }) => {
+  console.log(flatten(list));
+
+  const flattenList = useMemo(() => {
+    return flatten(list);
+  }, [list]);
+
+  return <VirtualList list={flattenList} {...props} />;
 };
 
 export default Comp;
